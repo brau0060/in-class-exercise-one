754b0f518bdb3e644057af138440da0b47fd6659
Alexander Braun <brau0060@algonquinlive.com>
2019-01-31 11:52:41 -0500

    what was done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld/>
   </div>
 </template>
 
@@ -11,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'app',
   components: {
+    /* eslint-disable */
     HelloWorld
   }
 }
